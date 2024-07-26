a5b0b59e6ea5bed44f4b2afc0f953bfd92567d45
akilaprabhavi <akilasiniki@gmail.com>
2024-07-26 12:41:41 +1000

    unit test code changes

--- test/project3.test.js
+++ test/project3.test.js
@@ -4,6 +4,30 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/project3-stack.js
+
+const { Template } = require('@aws-cdk/assertions');
+const { App } = require('aws-cdk-lib');
+const { Project3Stack } = require('../lib/project3-stack');
+
+let app, stack, template;
+
+beforeAll(() => {
+    app = new App();
+    stack = new Project3Stack(app, 'MyTestStack');
+    template = Template.fromStack(stack);
+});
+
+test('S3 Bucket Created', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketName: 'project3-canary-bucket',
+    });
+});
+
+test('S3 Deployment Configured', () => {
+    // Check for the custom resource for S3 deployment
+    template.resourceCountIs('Custom::CDKBucketDeployment', 1);
+});
+
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //   // WHEN
