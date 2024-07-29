861aa1865d021731f1003c95b736929a3aa8906f
akilaprabhavi <akilasiniki@gmail.com>
2024-07-29 22:25:15 +1000

    Application unit test cases

--- test/application.test.js
+++ test/application.test.js
@@ -15,6 +15,24 @@ const template = Template.fromStack(stack);
 
 // ======================================================== Unit/Assertions Test =====================================================
 
+// Test for lambda.Code.fromCfnParameters()
+test('Lambda Code is created from CloudFormation parameters', () => {
+    //template.resourceCountIs('AWS::CloudFormation::CustomResource', 1)
+})
+
+// Test for ssm.StringParameter.fromStringParameterName()
+test('SSM Parameter for Web Crawler Bucket Location is referenced', () => {
+    //template.hasResourceProperties('AWS::SSM::Parameter', {
+    //    Name: 'webcrawler-assets-bucket-location'
+    //})
+})
+
+test('S3 bucket is created', () => {
+    //template.hasResourceProperties('AWS::S3::Bucket', {
+    //   BucketName: 'webcrawler-assets-bucket-location'
+    //})
+})
+
 test('Web Crawler has been created', () => {
     template.hasResource("AWS::Lambda::Function", "");
 
@@ -22,12 +40,59 @@ test('Web Crawler has been created', () => {
         Handler: "webcrawler.handler"
     })
 })
+/*
+test('IAM Role for Web Crawler is created', () => {
+    template.hasResourceProperties('AWS::IAM::Role', {
+        AssumeRolePolicyDocument: {
+            Statement: [
+                {
+                    Action: 'sts:AssumeRole',
+                    Effect: 'Allow',
+                    Principal: {
+                        Service: 'lambda.amazonaws.com'
+                    }
+                }
+            ]
+        },
+        RoleName: 'webcrawler-iam-role-prod-ap-southeast-2'
+    })
+})
 
+test('IAM Role for Web Crawler has policies assigned', () => {
+    template.hasResourceProperties('AWS::IAM::Policy', {
+        PolicyDocument: {
+            Statement: [
+                {
+                    Action: ['s3:ListAllMyBuckets'],
+                    Effect: 'Allow',
+                    Resource: '*'
+                },
+                {
+                    Action: ['s3:*'],
+                    Effect: 'Allow',
+                    Resource: '*'
+                },
+                {
+                    Action: ['cloudwatch:PutMetricData'],
+                    Effect: 'Allow',
+                    Resource: '*'
+                },
+                {
+                    Action: ['logs:CreateLogStream', 'logs:CreateLogGroup', 'logs:PutLogEvents'],
+                    Effect: 'Allow',
+                    Resource: '*'
+                }
+            ]
+        }
+    });
+})
+*/
 test('Web Crawler runs every 5 minutes', () => {
     template.hasResourceProperties('AWS::Events::Rule', {
         ScheduleExpression: "rate(5 minutes)"
     })
 })
+
 
 test('There are 11 alarms, includes 3 Latency, 3 Availability, 3 BrokenLinks', () => {
     template.resourceCountIs("AWS::CloudWatch::Alarm", 12);
@@ -38,9 +103,101 @@ test('There are 11 alarms, includes 3 Latency, 3 Availability, 3 BrokenLinks', (
 })
 
 test('SNS has been created and has 2 email subscriptions', () => {
-    template.hasResource("AWS::SNS::Topic", "");
-    
+    template.hasResource("AWS::SNS::Topic", "");    
     template.resourceCountIs("AWS::SNS::Subscription", 2);
+})
+
+test('DynamoDB Table is created with correct properties', () => {
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        TableName: 'webcrawler-dynamodb-ap-southeast-2-prod',
+        AttributeDefinitions: [
+            { AttributeName: 'url', AttributeType: 'S' },
+            { AttributeName: 'timestamp', AttributeType: 'S' }
+        ],
+        KeySchema: [
+            { AttributeName: 'url', KeyType: 'HASH' },
+            { AttributeName: 'timestamp', KeyType: 'RANGE' }
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+        }
+    })
+})
+
+
+test('Lambda function has appropriate IAM role', () => {
+    template.hasResourceProperties('AWS::IAM::Role', {
+        Description: 'Lambda IAM Role for DynamoDB-ap-southeast-2-prod'
+        /*Policies: [
+            {
+               PolicyDocument: {
+                    Statement: [
+                        {
+                            Action: ['dynamodb:PutItem', 'dynamodb:UpdateItem', 'dynamodb:GetItem'],
+                        },
+                        {
+                            Action: ['logs:CreateLogStream', 'logs:CreateLogGroup', 'logs:PutLogEvents'],
+                        }
+                    ]
+                }
+            }
+        ]*/
+    })
+})
+
+test('IAM Role for Lambda interacting with DynamoDB is created', () => {
+    template.hasResourceProperties('AWS::IAM::Role', {
+        /*AssumeRolePolicyDocument: {
+            Statement: [
+                {
+                    Action: 'sts:AssumeRole',
+                    Effect: 'Allow',
+                    Principal: {
+                        Service: 'lambda.amazonaws.com'
+                    }
+                }
+            ],
+            //Version: '2012-10-17'
+        },*/
+        //Description: 'Lambda IAM Role for DynamoDB-ap-southeast-2-prod',
+        //RoleName: 'project3-lambda-dynamodb-role-ap-southeast-2-prod'
+    })
+})
+
+test('Lambda Function for DynamoDB is created', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        FunctionName: 'lambdadynamoDB-ap-southeast-2-prod',
+        Runtime: 'nodejs20.x',
+        Handler: 'dynamodb.handler'       
+    })
+})
+
+test('Lambda Alias for Web Crawler is created', () => {
+    //template.hasResourceProperties('AWS::Lambda::Alias', {
+    //    AliasName: 'prod'
+    // })
+})
+
+test('Lambda Deployment Group for Web Crawler is created', () => {
+    template.hasResourceProperties('AWS::CodeDeploy::DeploymentGroup', {
+        DeploymentConfigName: 'CodeDeployDefault.LambdaLinear10PercentEvery3Minutes'
+        /*AlarmConfiguration: {
+            Alarms: [
+                {
+                    Name: 'webcrawler-alarm-duration-ap-southeast-2-prod'
+                }
+            ],
+            Enabled: true
+        },
+        AutoRollbackConfiguration: {
+            Events: [
+                'DEPLOYMENT_FAILURE',
+                'DEPLOYMENT_STOP_ON_ALARM',
+                'DEPLOYMENT_STOP_ON_REQUEST'
+            ]
+        }*/
+    })
 })
 
 // ======================================================== Snapshot Test =====================================================
--- test/pipeline.test.js
+++ test/pipeline.test.js
@@ -11,7 +11,6 @@ const Sydney = {
 };
 
 const app = new cdk.App();
-
 const betaApplicationStack = new ApplicationStack(app, 'BetaApplicationStack', { env: Sydney, stackName: "BetaApplicationStack", stage: 'beta' });
 const prodApplicationStack = new ApplicationStack(app, 'ProdApplicationStack', { env: Sydney, stackName: "ProdApplicationStack", stage: 'prod' });
 
@@ -32,7 +31,129 @@ test('Bucket for webcrawler has been created', () => {
     })
 })
 
+test('Bucket has correct removal policy', () => {
+    template.hasResource('AWS::S3::Bucket', {
+      Properties: {
+        BucketName: 'project3-webcrawler-bucket'
+      },
+      DeletionPolicy: 'Delete' // This checks the `DeletionPolicy` for removal
+    })
+  })
 
 test('The pipeline has been created', () => {
     template.hasResource("AWS::CodePipeline::Pipeline", "");
 })
+
+test('S3 Bucket for pipeline has been created', () => {
+    template.hasResourceProperties('AWS::S3::Bucket', {
+        BucketName: 'project3-pipeline-bucket',
+        VersioningConfiguration: {
+            Status: 'Enabled',
+        },
+  })
+})
+
+test('S3 Bucket Deployment Configured', () => {
+    template.resourceCountIs('Custom::CDKBucketDeployment', 1);
+})
+
+test('SSM Parameter for webcrawler-assets-bucket-location has been created', () => {
+    //template.hasResourceProperties('AWS::SSM::Parameter', {
+    //  Name: 'webcrawler-assets-bucket-location',
+})
+
+/*
+test('GitHub Source Action has been created', () => {
+  template.hasResourceProperties('AWS::CodePipeline::Pipeline', {
+      Stages: [
+          {
+              Name: "Source",
+              Actions: [
+                 {                   
+                      Configuration: {
+                          Owner: githubOwner,
+                          Repo: githubRepo,
+                          Branch: githubBranch,
+                          OAuthToken: { "Ref": "GitHubToken" },
+                          PollForSourceChanges: false
+                     }                     
+                 }
+              ]
+          }
+      ]
+  })
+})
+*/
+
+test('Build CDK project has been created with correct properties', () => {
+    template.hasResourceProperties('AWS::CodeBuild::Project', {
+        Name: 'project3-cicd-build-cdk-project', 
+        Source: {
+            Type: 'CODEPIPELINE',
+        },
+        Artifacts: {
+            Type: 'CODEPIPELINE',
+        },
+        Environment: {
+            ComputeType: 'BUILD_GENERAL1_SMALL',
+            Image: 'aws/codebuild/standard:7.0',
+            Type: 'LINUX_CONTAINER',
+            ImagePullCredentialsType: 'CODEBUILD',
+        }  
+    })
+})
+/*
+test('CodeBuild Action for BuildCDK has been created with correct actionName', () => {
+    template.hasResourceProperties('AWS::CodePipeline::Pipeline', {
+        Stages: [
+            {
+                Actions: [
+                    {
+                        Name: 'BuildCDK',
+                        ActionTypeId: {
+                            Category: "Build",
+                            Owner: "AWS",
+                            Provider: "CodeBuild",
+                            Version: "1"
+                        }
+                    }
+                ]
+            }
+        ]
+    })
+})
+*/
+test('PipelineProject for Lambda build has been created', () => {
+    template.hasResource('AWS::CodeBuild::Project', {
+        Properties: {
+            Name: 'project3-cicd-build-lambda-project'
+        }
+    })
+})
+/*
+test('BuildSpec includes correct phases and artifacts', () => {
+    template.hasResourceProperties('AWS::CodeBuild::Project', {
+        BuildSpec: {
+            version: '0.2',
+            phases: {
+                install: {
+                    'runtime-versions': {nodejs: '20'},
+                    commands: ['cd src','cd lambdas']
+                }
+            },
+            artifacts: {
+                'base-directory': 'src/lambdas',
+                files: ['*.js']
+            }
+        }
+    })
+})
+
+test('CodeBuild Action has correct actionName', () => {
+    template.hasResourceProperties('AWS::CodePipeline::Pipeline', {
+        Stages: [
+            { Actions: [{ Name: "BuildLambda"}]}
+        ]
+    })
+})
+*/
